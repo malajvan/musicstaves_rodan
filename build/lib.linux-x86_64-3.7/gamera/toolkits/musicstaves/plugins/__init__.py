13dc9bc5c3464f1f2f698963b070c904f8bdca93
Van Pham <64571718+malajvan@users.noreply.github.com>
2022-06-07 20:58:58 +0000

    delete unrelated to miyao staff

--- build/lib.linux-x86_64-3.7/gamera/toolkits/musicstaves/plugins/__init__.py
+++ build/lib.linux-x86_64-3.7/gamera/toolkits/musicstaves/plugins/__init__.py
@@ -1,11 +1,3 @@
-import musicstaves_plugins
-import staff_removal_fujinaga
-import skewed_runs
-import line_tracking
+
 import staff_finding_miyao
-import skeleton_utilities
-import vector_field
-import evaluation
-import staffdeformation
-import stable_path
 
